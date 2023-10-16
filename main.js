bbdb26193b24f18775131361d0704a04f6df8dba
Srinivas <kolipakasrinivas456@gmail.com>
2023-10-16 23:10:48 +0530

    complated carousel cards  usning slick and little bit jquery

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-const openNavbar = () => document.body.classList.toggle('open')
+document.querySelector("#menu").addEventListener("click", function () {
+  document.body.classList.toggle("open");
+});
+
+document.querySelectorAll("a").forEach((a) => {
+  a.addEventListener("click", function () {
+    document.body.classList.remove("open");
+  });
+});
+
+window.addEventListener("resize", function () {
+  document.body.classList.remove("open");
+});
+
+// Define your scroll event function
+function onScroll() {
+  document.body.classList.remove("open");
+}
+
+// Attach a scroll event listener to the window
+window.addEventListener("scroll", onScroll);
